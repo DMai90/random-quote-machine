c1645ccbf4226d04536efe7dfbab61367edb0c10
David <dmai90@yahoo.com>
2018-12-13 20:10:26 -0800

    Updates

---
+++ precache-manifest.6abae59448cf24be8330518bbf8ab157.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8507572375d5b5ea7f9",
+    "url": "/random-quote-machine/static/js/runtime~main.b8507572.js"
+  },
+  {
+    "revision": "9231ccdc1c5be3a66ba9",
+    "url": "/random-quote-machine/static/js/main.9231ccdc.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/random-quote-machine/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "9231ccdc1c5be3a66ba9",
+    "url": "/random-quote-machine/static/css/main.2bd5beab.chunk.css"
+  },
+  {
+    "revision": "95632a2914cc36afc1e2876ad40b8b58",
+    "url": "/random-quote-machine/index.html"
+  }
+];
