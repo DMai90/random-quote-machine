de44a6deef05c352e55b4e48c0c317f4134e692a
DevMai90 <dmai90@yahoo.com>
2019-06-22 10:56:44 -0700

    Implemented async/await and destructured state

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -7,24 +7,27 @@ class QuoteMachine extends Component {
       author: ''
     }
   };
-  getNewQuote = e => {
-    fetch(
+  getNewQuote = async () => {
+    const res = await fetch(
       'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
-    )
-      .then(response => response.json())
-      .then(data => {
-        const { quotes } = data;
-        let number = Math.floor(Math.random() * quotes.length);
-        this.setState({
-          quote: {
-            content: quotes[number].quote,
-            author: quotes[number].author
-          }
-        });
-      });
+    );
+
+    const { quotes } = await res.json();
+
+    let number = Math.floor(Math.random() * quotes.length);
+
+    this.setState({
+      quote: {
+        content: quotes[number].quote,
+        author: quotes[number].author
+      }
+    });
   };
   render() {
-    const { quote } = this.state;
+    const {
+      quote: { content, author }
+    } = this.state;
+
     return (
       <div className="container">
         <div className="row ">
@@ -32,15 +35,17 @@ class QuoteMachine extends Component {
             <div className="card text-center" id="quote-box">
               <div className="card-header bg-primary text-white">
                 <div className="card-title">
-                  <h3>Random Quote Machine</h3>
+                  <h3 className="display-4 p-2" style={{ fontSize: '36px' }}>
+                    Random Quote Machine
+                  </h3>
                 </div>
               </div>
               <div className="card-body">
                 <blockquote className="blockquote" id="text">
-                  <p>{quote.content}</p>
+                  <p>{content}</p>
                 </blockquote>
                 <footer className="blockquote-footer" id="author">
-                  {quote.author}
+                  {author}
                 </footer>
 
                 <div className="col mt-3">
