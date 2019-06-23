a6ac588e35a906a363fa4e48502cb18da346195a
DevMai90 <dmai90@yahoo.com>
2019-06-22 22:54:47 -0700

    Added Spinner component

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -1,31 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Spinner from './Spinner';
 
 class QuoteMachine extends Component {
   state = {
     quote: {
       content: '',
       author: ''
-    }
+    },
+    loading: false
   };
+
   getNewQuote = async () => {
+    this.setState({ ...this.state, loading: true });
+
     const res = await fetch(
       'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
     );
-
     const { quotes } = await res.json();
 
     let number = Math.floor(Math.random() * quotes.length);
 
-    this.setState({
-      quote: {
-        content: quotes[number].quote,
-        author: quotes[number].author
-      }
-    });
+    setTimeout(() => {
+      this.setState({
+        quote: {
+          content: quotes[number].quote,
+          author: quotes[number].author
+        },
+        loading: false
+      });
+    }, 1000);
   };
+
   render() {
     const {
-      quote: { content, author }
+      quote: { content, author },
+      loading
     } = this.state;
 
     return (
@@ -40,32 +49,38 @@ class QuoteMachine extends Component {
                   </h3>
                 </div>
               </div>
-              <div className="card-body">
-                <blockquote className="blockquote" id="text">
-                  <p>{content}</p>
-                </blockquote>
-                <footer className="blockquote-footer" id="author">
-                  {author}
-                </footer>
+              {loading ? (
+                <Spinner />
+              ) : (
+                <Fragment>
+                  <div className="card-body">
+                    <blockquote className="blockquote" id="text">
+                      <p>{content}</p>
+                    </blockquote>
+                    <footer className="blockquote-footer" id="author">
+                      {author}
+                    </footer>
 
-                <div className="col mt-3">
-                  <a href="twitter.com/intent/tweet" id="tweet-quote">
-                    <i
-                      className="fab fa-twitter mr-3"
-                      style={{ fontSize: '30px' }}
-                    />
-                  </a>
+                    <div className="col mt-3">
+                      <a href="twitter.com/intent/tweet" id="tweet-quote">
+                        <i
+                          className="fab fa-twitter mr-3"
+                          style={{ fontSize: '30px' }}
+                        />
+                      </a>
 
-                  <button
-                    type="button"
-                    className="btn btn-primary"
-                    onClick={this.getNewQuote}
-                    id="new-quote"
-                  >
-                    Click for a new quote
-                  </button>
-                </div>
-              </div>
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={this.getNewQuote}
+                        id="new-quote"
+                      >
+                        Click for a new quote
+                      </button>
+                    </div>
+                  </div>
+                </Fragment>
+              )}
             </div>
           </div>
         </div>
---
+++ src/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div class="spinner-border text-primary mx-auto my-3" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  );
+};
+
+export default Spinner;
